29f618926443a660516d090f1377a25d8d702ff9
shivam123425 <shivam123425@gmail.com>
2019-06-01 17:40:30 +0530

    Updates

---
+++ precache-manifest.0068ba550220424dd3c0225e3531864f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd59a253d64464d5cf4338afcc216ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeff18716d5e97dbb2cf",
+    "url": "/static/css/main.bbecde6e.chunk.css"
+  },
+  {
+    "revision": "bd26e7f56ed00aae732e",
+    "url": "/static/js/2.af90c070.chunk.js"
+  },
+  {
+    "revision": "eeff18716d5e97dbb2cf",
+    "url": "/static/js/main.442bcb23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
